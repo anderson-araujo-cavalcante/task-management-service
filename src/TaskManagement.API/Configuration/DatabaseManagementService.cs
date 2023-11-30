2eafe63ec0eaebc4179c7489e4331fc294f3b820
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-11-30 19:27:27 -0300

    Create DatabaseManagementService.cs

---
+++ src/TaskManagement.API/Configuration/DatabaseManagementService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Data.Context;
+
+namespace TaskManagement.API.Configuration
+{
+    public static class DatabaseManagementService
+    {
+        public static void MigrationInitialisation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<TaskManagementContext>().Database.Migrate();
+            }
+        }
+    }
+}
