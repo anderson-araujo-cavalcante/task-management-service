8e8fa821e91322a05ae442f39a1ee58afd1c26b3
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-11-30 09:14:47 -0300

    add dbcontext

---
+++ src/TaskManagement.Data/Context/TaskManagementContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Entities;
+
+
+namespace TaskManagement.Data.Context
+{
+    public class TaskManagementContext : DbContext
+    {
+        public TaskManagementContext(DbContextOptions<TaskManagementContext> options) : base(options)
+        { }
+
+        public DbSet<ProjectTask> Tasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
