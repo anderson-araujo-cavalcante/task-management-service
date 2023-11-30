0c4cf7f4625fb99c0a14d733c458034107a12f8e
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-11-30 18:38:51 -0300

    add DependencyInjectionConfig

---
+++ src/TaskManagement.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using TaskManagement.Data.Context;
+using TaskManagement.Domain.Interfaces.Repositories;
+using TaskManagement.Domain.Interfaces.Services;
+
+namespace TaskManagement.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<TaskManagementContext>();
+
+            services.AddScoped<IProjectService, IProjectService>();
+
+            services.AddScoped<IProjectTaskRepository, IProjectTaskRepository>();
+
+            return services;
+        }
+    }
+}
