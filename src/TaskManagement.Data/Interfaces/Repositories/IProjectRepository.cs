9c3111c1572794218e4140663b1ca9c7e9e0504c
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-11-30 09:24:23 -0300

    add repository interfaces

---
+++ src/TaskManagement.Data/Interfaces/Repositories/IProjectRepository.cs
@@ -0,0 +1,8 @@
+﻿using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Data.Interfaces.Repositories
+{
+    public interface IProjectRepository : IRepositoryBase<Project>
+    {
+    }
+}
---
+++ src/TaskManagement.Data/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagement.Data.Interfaces.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity obj);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(int entity);
+        void Dispose();
+    }
+}
