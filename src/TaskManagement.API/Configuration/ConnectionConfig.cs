5db3d3eb43915225dbb7a8030b993dd55c441dc3
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-11-30 19:12:13 -0300

    add DbContext config

---
+++ src/TaskManagement.API/Configuration/ConnectionConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Data.Context;
+
+namespace TaskManagement.API.Configuration
+{
+    public static class ConnectionConfig
+    {
+        public static WebApplicationBuilder AddDbContext(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContextPool<TaskManagementContext>(
+            options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)
+            ));
+
+            return builder;
+        }
+    }
+}
--- src/TaskManagement.API/Program.cs
+++ src/TaskManagement.API/Program.cs
@@ -1,3 +1,5 @@
+using TaskManagement.API.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ResolveDependencies();
+builder.AddDbContext();
 
 var app = builder.Build();
 
