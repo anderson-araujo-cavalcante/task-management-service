e4c4067005a686b55d2df7d9b43f82b20401f6f7
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-11-30 09:19:00 -0300

    add entity type configuration map

---
+++ src/TaskManagement.Data/Mappings/ProjectMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Data.Mappings
+{
+    public class ProjectMap : IEntityTypeConfiguration<Project>
+    {
+        private const string TABLE_NAME = "Projects";
+
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable(TABLE_NAME);
+
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(c => c.UserId)
+            .IsRequired();
+        }
+    }
+}
---
+++ src/TaskManagement.Data/Mappings/ProjectTaskMap.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Data.Mappings
+{
+    public class ProjectTaskMap : IEntityTypeConfiguration<ProjectTask>
+    {
+        private const string TABLE_NAME = "Tasks";
+
+        public void Configure(EntityTypeBuilder<ProjectTask> builder)
+        {
+            builder.ToTable(TABLE_NAME);
+
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(c => c.ExpirationDate)
+                .IsRequired();
+
+            builder.HasOne(u => u.Project)
+                .WithMany()
+                .HasForeignKey(u => u.ProjectId)
+                .IsRequired(true);
+        }
+    }
+}
